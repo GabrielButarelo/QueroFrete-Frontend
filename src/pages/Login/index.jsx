f9ea0150fa3fe02e8dd805c7c5b3f38a76efa152
Gabriel Butarelo <gabrielbutarelo16@gmail.com>
2022-02-06 23:38:13 -0300

    change class components to function components

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,80 +1,76 @@
 import './styles.css';
-import React from 'react';
+import { React, useState } from 'react';
 import axios from 'axios';
 import { Navigate } from "react-router-dom";
 import FlashMessage from '../../components/FlashMessage';
 
-class Login extends React.Component {
+function Login() {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [logged, setLogged] = useState(false);
 
-  state = {
-    password: '',
-    email: '',
-    isEmpty: false,
-    hasError: false,
-    errorMessage: '',
-    logged: false,
-  }
-
-  async handleSubmit() {
-    const { password, email } = this.state;
-
+  const handleSubmit = async () => {
     setTimeout(() => {
-      this.setState({ hasError: false, errorMessage: '' });
+      setHasError(false)
+      setErrorMessage('')
     }, 20000);
 
     if (!password.length || !email.length) {
-      this.setState({ isEmpty: true });
+      setIsEmpty(true)
       return;
     } else if (password.length || email.length) {
-      this.setState({ isEmpty: false });
+      setIsEmpty(false)
     }
 
     const { data } = await axios.get(`http://localhost:3003/main/login/?password=${password}&email=${email}`);
 
     switch (data.status) {
       case 200:
-        this.setState({ logged: true });
+        setLogged(true)
         return;
 
       case 400:
-        this.setState({ hasError: true, errorMessage: data.message });
+        setHasError(true)
+        setErrorMessage(data.message)
         return;
+
+      default:
+        setHasError(true)
+        setErrorMessage('Erro desconhecido, entre em contato com o suporte!')
     }
   }
 
-  handleChangeEmail(e) {
-    this.setState({ email: e.target.value });
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
   }
 
-  handleChangePassword(e) {
-    this.setState({ password: e.target.value });
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
   }
 
-  render() {
-    const { isEmpty, hasError, errorMessage, logged } = this.state;
-
-    return (
-      <div className="container">
-        {!!logged && (<Navigate to={'/painel'} />)}
-        <div className="login">
-          <h1>QueroFrete</h1>
-          <p>A tecnologia que conecta!</p>
-          <div className="form">
-            <input type="email" onChange={(e) => this.handleChangeEmail(e)} />
-            <input type="password" onChange={(e) => this.handleChangePassword(e)} />
-            <a href="#">
-              Esqueci minha senha
-            </a>
-            {!!isEmpty && (<FlashMessage duration={20} message='O email e a senha precisam ser preenchidos!' type='error' />)}
-            {!!hasError && (<FlashMessage duration={20} message={errorMessage} type='error' />)}
-            <button onClick={() => this.handleSubmit()}>Entrar</button>
-          </div>
-          <p>Não possui conta? <a href="#">Clique aqui</a> para criar uma.</p>
+  return (
+    <div className="container">
+      {!!logged && (<Navigate to={'/painel'} />)}
+      <div className="login">
+        <h1>QueroFrete</h1>
+        <p>A tecnologia que conecta!</p>
+        <div className="form">
+          <input type="email" onChange={(e) => handleChangeEmail(e)} />
+          <input type="password" onChange={(e) => handleChangePassword(e)} />
+          <a href="#">
+            Esqueci minha senha
+          </a>
+          {!!isEmpty && (<FlashMessage duration={20} message='O email e a senha precisam ser preenchidos!' type='error' />)}
+          {!!hasError && (<FlashMessage duration={20} message={errorMessage} type='error' />)}
+          <button onClick={() => handleSubmit()}>Entrar</button>
         </div>
+        <p>Não possui conta? <a href="#">Clique aqui</a> para criar uma.</p>
       </div>
-    );
-  }
-
+    </div>
+  );
 }
 
 export default Login;
